2b799a252dd4acc055938fba0aa38ff66f43b88a
Jason Paris <paris3200@gmail.com>
2015-11-09 22:56:21 -0500

    added website url

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='paris3200@gmail.com',
     package='wunderapi',
     package_dir={'': 'wunderapi'},
-    url='github',
+    url='https://github.com/paris3200/wunderapi',
     install_requires=['Click', 'Requests'],
     test_suite='nose.collector',
     test_requires=['coverage', 'nose'],
