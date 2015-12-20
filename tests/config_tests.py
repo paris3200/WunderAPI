ed57046c8b5080602ce04886dca36ca5740796a4
Jason Paris <paris3200@gmail.com>
2015-12-19 21:20:50 -0500

    added config test

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -1,15 +1,23 @@
-from nose.tools import *
+from nose.tools import assert_equal
 from wunderapi.config import Config
 
-def setup():
-    pass
 
-def test_create_config_set_units():
-    pass
+def setup():
+    return Config(config_file="tests/resources/test_config")
+
 
 def test_parse_config_with_correct_parms():
     pass
 
+
 def test_parse_config_with_incorrect_parms():
     pass
 
+
+def test_config_created_with_default_parms():
+    config = setup()
+    config.parse_config()
+    assert_equal(config.api_key, 'API Key')
+    assert_equal(config.location, 'Zipcode')
+    assert_equal(config.date_format, 'date')
+    assert_equal(config.units, 'english')
